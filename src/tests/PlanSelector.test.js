caf4c7946c572502dac99915c9329e6632d2369f
Lawrence Li <lilawren@Lawrences-MacBook-Pro-2.local>
2020-02-11 23:41:43 -0800

    add more tests

--- src/tests/PlanSelector.test.js
+++ src/tests/PlanSelector.test.js
@@ -3,15 +3,15 @@ import { mount } from 'enzyme';
 import PlanSelector from '../components/PlanSelector';
 
 describe('[UNIT] Testing the PlanSelector component', () => {
+  let initialProps = {
+    currentPlan: 'good',
+    currentSeats: 5,
+    cost: 50,
+  };
+
   let wrapper;
 
   describe('Initial render', () => {
-    let initialProps = {
-      currentPlan: 'good',
-      currentSeats: 5,
-      cost: 50,
-    };
-
     beforeEach(() => {
       wrapper = mount(<PlanSelector {...initialProps} />);
     });
@@ -34,7 +34,45 @@ describe('[UNIT] Testing the PlanSelector component', () => {
     });
   });
 
-  describe('Loading render', () => {
-    it('should display the loader when isLoadingCost is true', () => {});
+  describe('Loading spinner', () => {
+    beforeEach(() => {
+      wrapper = mount(
+        <PlanSelector
+          {...{
+            ...initialProps,
+            isLoadingCost: true,
+          }}
+        />
+      );
+    });
+
+    it('should display the loader when isLoadingCost is true', () => {
+      expect(wrapper.find('div.ui.active.inline.loader').exists());
+    });
+  });
+
+  describe('Trigger plan cost recalculation', () => {
+    let baseProps = {
+      ...initialProps,
+      onSeatsChange: jest.fn(),
+    };
+
+    beforeEach(() => {
+      wrapper = mount(<PlanSelector {...baseProps} />);
+    });
+
+    it('should call props function onSeatsChange when user blurs the seats input', () => {
+      let input = wrapper.find('input');
+      input.simulate('change', {
+        target: { value: '12' },
+      });
+
+      input.simulate('blur');
+      expect(baseProps.onSeatsChange).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call props function onSeatsChange when user presses enter when focused on seats input', () => {});
+
+    it('should call props function onPlanChange when user changes the plan input', () => {});
   });
 });
